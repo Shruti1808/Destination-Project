b81ba59b269274b951e637d136e2455f8e9c1f28
Sam Kirsch and Shruti Priya <shruti@>
2017-01-23 11:28:46 -0800

    add business and user logic to get the functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,54 @@
-var Destination = function(locationName, landmarks, timeOfYear, notes) {
+var Destination = function(locationName, timeOfYear, notes) {
   this.locationName = locationName;
-  this.landmarks = landmarks;
+  this.landmarks = [];
   this.timeOfYear = timeOfYear;
   this.notes = notes;
 }
 
 
+
+
+
+
+
+
+
+
+
+// user interface logic here
 $(document).ready(function() {
   $("form#new-location").submit(function(event) {
     event.preventDefault();
+    var inputtedLocationName = $("input#new-location-name").val();
+    var inputtedTimeOfYear = $("input#time-of-year").val();
+    var inputtedNote = $("input#note").val();
 
+    var newDestination = new Destination(inputtedLocationName, inputtedTimeOfYear,inputtedNote);
+
+    $(".new-landmark").each(function() {
+      var inputtedLandmark = $("input#landmark").val();
+      newDestination.landmarks.push(inputtedLandmark);
+    });
+
+
+    $("ul#locations-list").append("<li><span class='location-name'>" + newDestination.locationName + "</span></li>");
+    $("#locations-list").last().click(function() {
+      console.log("clicked!");
+      $(".output-container").show();
+      $(".place").text(newDestination.locationName);
+      $(".time").text(newDestination.timeOfYear);
+      $(".note-text").text(newDestination.notes);
+      for (i=0; i < newDestination.landmarks.length; i++) {
+        var newLandmark = newDestination.landmarks[i];
+        $("ul#landmark-list").append("<li><span class='location-name'>" + newLandmark + "</span></li>");
+        console.log(newLandmark);
+      }
+    })
+
+    $("input#new-location-name").val("");
+    $("input#landmark").val("");
+    $("input#time-of-year").val("");
+    $("input#note").val("");
   });
 
   $("#add-landmark").click(function() {
